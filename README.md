f97ce7d624163b2fc91e6a6b1c11e3863e47dfe9
Travis CI <aschmitt@berkeley.edu>
2016-01-14 16:05:20 -0500

    updated README. Added more configuration

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
 # hubot-google-auth
-Hubot functions and utility functions for authenticating to google apis
+Hubot utility functions for authenticating to google apis
+
+Check out an example script [here](https://github.com/aaaschmitt/hubot-googledrive-search/blob/master/src/hubot-googledrive-search.js).
+
+## Install
+```
+npm install --save hubot-google-auth
+```
+
+## Configuration
+
+You will need to generate a client\_secret.json file to authenticate via google's oauth2 protocol. Instructions for doing this can be found Generate a client secret.json file by following **step 1** [here](https://developers.google.com/drive/v2/web/quickstart/nodejs).
+
+Using your client\_secret.json you can initialize the HubotGoogleAuth object like this:
+```
+HubotGoogleAuth = require 'hubot-google-auth'
+
+# Note that we need acess to the robot's brain to init the auth object
+# Scopes should be a text string of scope urls, each seperated by a semicolon (;)
+# Service name should be the google service that is being used or that you would like to share tokens between bots for
+auth = new HubotGoogoleAuth ServiceName, CLIENT_ID, CLIENT_SECRET, REDIRECT_URL, SCOPES, robot.brain
+```
+
+## Usage
+Inteneded be be used by hubot scripts to perform google auth by storing api tokens in the brain.  This is useful because it allows you to require this module in several different scripts that need to use google auth (to a particular service that name that is chosen) and they will retrieve and set tokens into the brain when needed.
+
+## Sample script
+
+See an extened example script [here](https://github.com/aaaschmitt/hubot-googledrive-search/blob/master/src/hubot-googledrive-search.js). Check out the package that it comes from [here](https://www.npmjs.com/package/hubot-googledrive-search).
+
+```
+CLIENT_ID = process.env.HUBOT_DRIVE_CLIENT_ID
+CLIENT_SECRET = process.env.HUBOT_DRIVE_CLIENT_SECRET
+REDIRECT_URL = process.env.HUBOT_DRIVE_REDIRECT_URL
+SCOPES = 'https://www.googleapis.com/auth/drive'
+
+HubotGoogleAuth = require 'hubot-google-auth'
+
+module.exports = (robot) ->
+	
+	# We need to initialize the auth client here because it is initialzed with the brain
+    auth = new HubotGoogleAuth "GoogleDrive", CLIENT_ID, CLIENT_SECRET, REDIRECT_URL, SCOPES, robot.brain
+
+    # Tokens be null initially
+    tokens = auth.getTokens()
+
+    # Google apis are exposed via the google object
+    # See the googleapis npm package for how to use these apis
+    drive = auth.google.drive('v2')
+
+    robot.respond /show tokens/i, (msg)->
+
+    	if !tokens.token
+    		msg.send "No tokens found"
+    		msg.send "Please copy the code at this url #{auth.generateAuthUrl()}"
+    		msg.send "Then use the command @hubot set code <code>"
+    		return
+
+    	msg.send auth.getTokens()
+
+    robot.respond /set code (.+)/i, (msg)->
+    	code = msg.match[1]
+
+    	# Gets new token and refresh token and stores into brain
+    	auth.setCode code, (err, resp)->
+    		if err
+    			msg.send "Could not obtain tokens with code: #{code}"
+    			return
+
+    		msg.send "Code successfully set. Tokens now stored in brain for service: #{auth.serviceName}"
+
+    robot.respond /drive request/i, (msg)->
+		drive.files.get {fileId: 'some_id'}, (err, resp)->
+			if err
+				return msg.send "ERROR: could not get file: #{err}"
+
+			msg.send resp.title
+```	
--- auth.js
+++ auth.js
@@ -7,27 +7,49 @@
 // Author:
 //   Andrew Schmitt
 var google = require('googleapis');
-
-// Make sure that these keys do not conflict with things that are already in your hubot's brain
-var TOKEN_KEY = 'GOOGLE_AUTH_TOKEN',
-    REFRESH_KEY = 'GOOGLE_AUTH_REFRESH_TOKEN',
-    EXPIRY_KEY = 'GOOGLE_AUTH_EXPIRE_TIME';
-
+var OAuth2 = google.auth.OAuth2;
 
 /**
  * Initialzie the module will all the necessary google information 
  * along with the brain of your robot.
  * This module will then store tokens into your robot's brain for you using the keys above.
+ *
+ * @param  serviceName   the name of google service this client should use. 
+ *                       Keys names for the brain are generated based on service name.
+ *                       This allows scripts to share the same service or use different services
+ * @param  clientId      string, found in your client_secret.json
+ * @param  clientSecret  string, found in your client_secret.json
+ * @param  redirectUrl   string, found in your client_secret.json
+ * @param  scopes        string of scope urls, separated by semicolons
+ * @param  brain         your robot's brain
  */
-function HubotGoogleAuth(clientId, clientSecret, redirectUrl, scopes, brain) {
-    var OAuth2 = google.auth.OAuth2;
-    this.oauthClient = new OAuth2(clientId, clientSecret, redirectUrl);
+function HubotGoogleAuth(serviceName, clientId, clientSecret, redirectUrl, scopes, brain) {
+    var client = new OAuth2(clientId, clientSecret, redirectUrl),
+        tok_key = serviceName + '_GOOGLE_AUTH_TOKEN',
+        ref_key = serviceName + '_GOOGLE_AUTH_REFRESH_TOKEN',
+        exp_key = serviceName + '_GOOGLE_AUTH_EXPIRE_TIME';
+
+    // may be null initially. It is your scipts job to hanlde initial app authorization
+    // See generateAuthUrl and setCode
+    client.setCredentials({
+        access_token: brain.get(tok_key),
+        refresh_token: brain.get(ref_key)
+    });
+
+    // Tells the google object to use this client to authenticate all requests
+    google.options({
+        auth: client
+    });
+
+    this.oauthClient = client
     this.scopes = scopes;
     this.google = google;
-    google.options({
-        auth: this.oauthClient
-    })
-    this.brain = brain;
+
+    // Make sure that these keys do not conflict with things that are already in your hubot's brain
+    this.TOKEN_KEY = tok_key;
+    this.REFRESH_KEY = ref_key;
+    this.EXPIRY_KEY = exp_key;
+    this.serviceName = serviceName;
 }
 
 HubotGoogleAuth.prototype = {
@@ -40,11 +62,11 @@ HubotGoogleAuth.prototype = {
      */
     storeToken: function(token) {
         this.oauthClient.setCredentials(token);
-        this.brain.set(TOKEN_KEY, token.access_token);
+        this.brain.set(this.TOKEN_KEY, token.access_token);
         if (token.refresh_token) {
-            this.brain.set(REFRESH_KEY, token.refresh_token);
+            this.brain.set(this.REFRESH_KEY, token.refresh_token);
         }
-        this.brain.set(EXPIRY_KEY, +token.expiry_date);
+        this.brain.set(this.EXPIRY_KEY, +token.expiry_date);
         this.brain.save();
         this.brain.resetSaveInterval(60);
     },
@@ -53,6 +75,10 @@ HubotGoogleAuth.prototype = {
      * Initially tokens must be created from the command line.
      * This requires a user manually inputting a code so it cannot be done by the bot alone.
      * This generates the url where the code can be obtained
+     *
+     * Generally a script should check if a token exists by calling auth.getTokens() below.
+     * If no tokens are found then generate this auth url and send a message to the user
+     * The user visits the auth url and then sets the code using the setCode function below
      */
     generateAuthUrl: function() {
 
@@ -66,7 +92,7 @@ HubotGoogleAuth.prototype = {
 
     /**
      * Used to set the code provided by the generated auth url. 
-     * This code is generated for a user and is needed to initiate the oauth2 handshake.
+     * This code is generated for a user and is needed to initiate the oauth2 handshake
      *
      * @param  code  the code obtained by a user from the auth url
      */
@@ -96,8 +122,8 @@ HubotGoogleAuth.prototype = {
      * @param  cb  the callback function (err, resp), use this to make api calls
      */
     validateToken: function(brain, cb) {
-        var at = this.brain.get(TOKEN_KEY),
-            rt = this.brain.get(REFRESH_KEY);
+        var at = this.brain.get(this.TOKEN_KEY),
+            rt = this.brain.get(this.REFRESH_KEY);
 
         if (at == null || rt == null) {
 
@@ -108,7 +134,7 @@ HubotGoogleAuth.prototype = {
             return;
         }
 
-        var expirTime = this.brain.get(EXPIRY_KEY),
+        var expirTime = this.brain.get(this.EXPIRY_KEY),
             curTime = (new Date()) / 1;
 
         var self = this;
@@ -138,9 +164,9 @@ HubotGoogleAuth.prototype = {
      */
     getTokens: function() {
         return {
-            token: this.brain.get(TOKEN_KEY),
-            refresh_token: this.brain.get(REFRESH_KEY),
-            expire_date: this.brain.get(EXPIRY_KEY)
+            token: this.brain.get(this.TOKEN_KEY),
+            refresh_token: this.brain.get(this.REFRESH_KEY),
+            expire_date: this.brain.get(this.EXPIRY_KEY)
         }
     }
 
